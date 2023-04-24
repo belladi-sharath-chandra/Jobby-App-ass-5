555b2f2776ab5813b5151dfef052bffe81c9cda5
belladi-sharath-chandra <bsharathchandra9@gmail.com>
2023-04-24 08:09:12 +0000

    jobby app

---
+++ src/components/Header/index.js
@@ -0,0 +1,83 @@
+import {Link, withRouter} from 'react-router-dom'
+
+import Cookies from 'js-cookie'
+
+import {AiFillHome} from 'react-icons/ai'
+import {BsFillBriefcaseFill} from 'react-icons/bs'
+import {FiLogOut} from 'react-icons/fi'
+import './index.css'
+
+const Header = props => {
+  const onClickLogout = () => {
+    const {history} = props
+    Cookies.remove('jwt_token')
+    history.replace('/login')
+  }
+
+  return (
+    <nav className="nav-header">
+      <div className="nav-content">
+        <div className="nav-bar-mobile-logo-container">
+          <Link to="/">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
+              alt="website-logo"
+              className="website-logo"
+            />
+          </Link>
+          <ul className="nav-bar-mobile-icons-container">
+            <li>
+              <Link to="/">
+                <AiFillHome className="nav-item-mobile-link" />
+              </Link>
+            </li>
+            <li>
+              <Link to="/">
+                <BsFillBriefcaseFill className="nav-item-mobile-link" />
+              </Link>
+            </li>
+            <li>
+              <button
+                className="nav-mobile-btn"
+                onClick={onClickLogout}
+                type="button"
+              >
+                <FiLogOut />
+              </button>
+            </li>
+          </ul>
+        </div>
+        <div className="nav-bar-large-container">
+          <Link to="/">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
+              className="website-logo"
+              alt="website logo"
+            />
+          </Link>
+          <ul className="nav-menu">
+            <li className="nav-menu-item">
+              <Link to="/" className="nav-link">
+                Home
+              </Link>
+            </li>
+            <li className="nav-menu-item">
+              <Link to="/jobs" className="nav-link">
+                Jobs
+              </Link>
+            </li>
+          </ul>
+          <button
+            className="logout-desktop-btn"
+            type="button"
+            onClick={onClickLogout}
+          >
+            Logout
+          </button>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default withRouter(Header)
---
+++ src/components/Login/index.js
@@ -0,0 +1,97 @@
+import {Component} from 'react'
+import Cookies from 'js-cookie'
+import {Redirect} from 'react-router-dom'
+import './index.css'
+
+class Login extends Component {
+  state = {username: '', password: '', showSubmitError: false, errorMsg: ''}
+
+  onChangeUsername = event => {
+    this.setState({username: event.target.value})
+  }
+
+  onChangePassword = event => {
+    this.setState({password: event.target.value})
+  }
+
+  onSubmitSuccess = jwtToken => {
+    const {history} = this.props
+    Cookies.set('jwt_token', jwtToken, {expires: 30})
+    history.replace('/')
+  }
+
+  onSubmitFailure = errorMsg => {
+    this.setState({errorMsg, showSubmitError: true})
+  }
+
+  submitForm = async event => {
+    event.preventDefault()
+    const {username, password} = this.state
+    const userDetails = {username, password}
+    const apiUrl = 'https://apis.ccbp.in/login'
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(userDetails),
+    }
+    const response = await fetch(apiUrl, options)
+    const data = await response.json()
+
+    if (response.ok === true) {
+      this.onSubmitSuccess(data.jwt_token)
+    } else {
+      this.onSubmitFailure(data.error_msg)
+    }
+  }
+
+  render() {
+    const {username, password, showSubmitError, errorMsg} = this.state
+    const token = Cookies.get('jwt_token')
+    if (token !== undefined) {
+      return <Redirect to="/" />
+    }
+
+    return (
+      <div className="login-container">
+        <form className="form-container" onSubmit={this.submitForm}>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
+            alt="website logo"
+            className="login-website-logo"
+          />
+          <div className="input-container">
+            <label className="input-label" htmlFor="username">
+              USERNAME
+            </label>
+            <input
+              type="text"
+              id="username"
+              className="username-input-field"
+              value={username}
+              placeholder="Username : rahul"
+              onChange={this.onChangeUsername}
+            />
+          </div>
+          <div className="input-container">
+            <label className="input-label" htmlFor="password">
+              PASSWORD
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="password-input-field"
+              value={password}
+              placeholder="Password : rahul@2021"
+              onChange={this.onChangePassword}
+            />
+          </div>
+          <button className="login-button" type="submit">
+            Login
+          </button>
+          {showSubmitError && <p className="error-message">*{errorMsg}</p>}
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Login
